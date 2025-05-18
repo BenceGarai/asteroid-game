4988391cb2db2290491f140ae1e7f888e1418971
Bence Garai <garaibence@gmail.com>
2025-05-18 10:48:09 -0700

    Add player and player turning

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
@@ -10,13 +11,18 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-   
+
+    player_1 = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill((0,0,0))
+        screen.fill("black")
+        player_1.update(dt)
+        player_1.draw(screen)
+        
         pygame.display.flip()
 
         # fps = 60
