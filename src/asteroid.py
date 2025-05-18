303cae96a6d08493d6e7ef79bd3811a50314c371
Bence Garai <garaibence@gmail.com>
2025-05-18 12:43:43 -0700

    Add splitting to asteroids on collision with a bullet

--- src/asteroid.py
+++ src/asteroid.py
@@ -1,5 +1,7 @@
-from circleshape import CircleShape
 import pygame
+import random
+from circleshape import CircleShape
+from constants import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +13,16 @@ class Asteroid(CircleShape):
     def update(self, dt):
         self.position += self.velocity * dt
     
+    def split(self):
+        self.kill()
+        # Smallest possible size already
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+
+        angle = random.uniform(20, 50)
+        asteroid_1.velocity = self.velocity.rotate(angle) * 1.2
+        asteroid_2.velocity = self.velocity.rotate(-angle) * 1.2
--- src/main.py
+++ src/main.py
@@ -44,6 +44,11 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+            for shot in shots:
+                if asteroid.checkCollision(shot):
+                    shot.kill()
+                    asteroid.split()
+
         screen.fill("black")
         
         for obj in drawable:
